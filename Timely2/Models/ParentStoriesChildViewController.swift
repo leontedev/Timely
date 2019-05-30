a03262697f83fb7c30c1f4548e366f39ea1a27dd
Mihail87 <leontemihai@gmail.com>
2019-05-30 11:20:20 +0300

    When the tab bar is tapped on the active tab, it navigates from the comments back to the master view controller (stories view).

---
+++ Timely2/Models/ParentStoriesChildViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ParentStoriesChildViewController.swift
+//  Timely2
+//
+//  Created by Mihai Leonte on 5/29/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import Foundation
